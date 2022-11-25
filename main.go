02de1b226f0bde9138c9f2e80e50d84d72a16837
GoSeoTaxi <95137583+GoSeoTaxi@users.noreply.github.com>
2022-11-25 14:21:29 +0300

    v1

--- main.go
+++ main.go
@@ -1,64 +1,84 @@
+//Memory cacher key-value
+// [string]interface{}
+
 package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type Cacher interface {
-	Get(string) (string, bool)
-	Set(string, string)
+	Get(string) (interface{}, bool)
+	Set(string, interface{})
 }
 
-// Тут надо дописать
 type Cache struct {
-	CacherTTL time.Duration
+	cacherTTL time.Duration
+	storage   *sync.Map
 }
 
-// Конструктор, тут надо дописать
 func NewCache(ttl time.Duration) Cacher {
-	m = make(map[string]data)
-	return Cache{CacherTTL: ttl}
+	sm := sync.Map{}
+	go func(m *sync.Map) {
+		for {
+			time.Sleep(ttl + (1 * time.Second))
+			m.Range(func(key interface{}, value interface{}) bool {
+				if time.Now().After(value.(data).timeTTL) {
+					m.Delete(key)
+				}
+				return true
+			})
+			fmt.Println(`clean complete`)
+		}
+	}(&sm)
+	return Cache{cacherTTL: ttl, storage: &sm}
 }
 
-var m map[string]data
-
-func (c Cache) Set(k1 string, v1 string) {
-	m[k1] = data{value: v1, timeTTL: time.Now().Add(c.CacherTTL)}
+func (c Cache) Set(key string, value interface{}) {
+	c.storage.Store(key, data{value: value, timeTTL: time.Now().Add(c.cacherTTL)})
 }
 
-func (c Cache) Get(k1 string) (string, bool) {
-	if !time.Now().After(m[k1].timeTTL) {
-		return m[k1].value, true
+func (c Cache) Get(k1 string) (interface{}, bool) {
+	item, ok := c.storage.Load(k1)
+	if ok && !time.Now().After(item.(data).timeTTL) {
+		return item.(data).value, true
 	}
-
-	return m[k1].value, false
+	return "", false
 }
 
 type data struct {
-	value   string
+	value   interface{}
 	timeTTL time.Time
 }
 
 func main() {
 	newCache := NewCache(5 * time.Second)
+
 	// пустое значение
 	v, ok := newCache.Get("test")
 	fmt.Println(v)
 	fmt.Println(ok)
-	fmt.Println(`+++++`)
-	newCache.Set("test", "test1")
+	fmt.Println(`Print "" false`)
+
 	// не пустое значение
+	newCache.Set("test", "test1")
+	newCache.Set("test2", "test2")
+	newCache.Set("test3", "test3")
+
 	time.Sleep(1 * time.Second)
+	newCache.Set("test5", "test5")
+
 	v, ok = newCache.Get("test")
 	fmt.Println(v)
 	fmt.Println(ok)
-	fmt.Println(`+++++`)
+	fmt.Println(`Print false "true"`)
 	// не пустое значение через время
 	time.Sleep(5 * time.Second)
 	v, ok = newCache.Get("test")
 	fmt.Println(v)
 	fmt.Println(ok)
-	fmt.Println(`+++++`)
+	fmt.Println(`Print "" false`)
 
 }
